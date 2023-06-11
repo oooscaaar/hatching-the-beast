4c21f8bf51264c6cf4d9d430e15cd167e6aa372c
Oscar Mellado <oscar-mellado@uoc.edu>
2023-06-11 22:59:29 +0200

    Add gitignore

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@onready var game_level = preload("res://scenes/game_level/game_level.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var _level = game_level.instantiate()
+	add_child(_level)
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
