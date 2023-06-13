61ce6c9ae9a2b3e050b5e8a77cc42ea02f79606c
Oscar Mellado <oscar-mellado@uoc.edu>
2023-06-13 12:08:33 +0200

    Finish composing parallax

--- scenes/game_level/_debug_fps.gd
+++ scenes/game_level/_debug_fps.gd
@@ -1,7 +1,5 @@
 extends Label
 
-@onready var btn_was_pressed: bool = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -9,5 +7,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.text = 'FPS: ' + str(Engine.get_frames_per_second())
-
+	self.text = str(Engine.get_frames_per_second())
