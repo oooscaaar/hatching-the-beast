9e94f9c66c067530a75be71fc05bf6d7c9ee3b72
Oscar M <empanaisimo@gmail.com>
2023-06-15 10:18:25 +0200

    WIP: Add player and platforms

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
-@onready var _player = $PlayerTexture
 @onready var _ground_raycast = $GroundRaycast
+@onready var _animated_player = $AnimatedPlayer
 
 var is_running: bool = false
 var is_grounded: bool = false
@@ -17,15 +17,15 @@ func _process(delta):
 
 	if is_grounded:
 		is_running = true
-		#_animated_sprite.play("run")
+		_animated_player.play("run")
 	else:
 		pass
-		#_animated_sprite.play("jump")
+		_animated_player.play("jump")
 		
 	
 	if Input.is_action_just_pressed("jump") and is_grounded:
 		print('Apply impulse!')
-		apply_impulse(Vector2(0, -300))
+		apply_impulse(Vector2(0, -250))
 		
 func stop_animation():
 	# _animated_sprite.stop()
---
+++ scenes/player/player_character_body.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const JUMP_VELOCITY = -160.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		$AnimatedPlayer.play("jump")
+	else:
+		$AnimatedPlayer.play("run")
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
