85547cd23c1149b8c4679466263b693014cc1c2a
Oscar M <empanaisimo@gmail.com>
2023-06-17 15:52:10 +0200

    fix: platform sprites

--- scenes/game_level/game_level.gd
+++ scenes/game_level/game_level.gd
@@ -3,29 +3,28 @@ extends Node2D
 @onready var player := $ParallaxBackground/Player
 @onready var parallax := $ParallaxBackground
 @onready var timer  := $Timer
-@onready var overlay := $Layout/AlphaLayers
+@onready var countdown := $Layout/Countdown
 @onready var animated_egg := $Layout/AnimatedEgg
-@onready var countdown_label := $Layout/CountDownLabel
+@onready var countdown_label := $Layout/Countdown/CountDownLabel
 
 
 func _ready():
 	set_processes([parallax], false)
 	animated_egg.stop()
 	timer.start()
-	overlay.show()
-	
-func start_game() -> void:
-	overlay.hide()
-	countdown_label.hide()
-	set_processes([parallax], true)
-	animated_egg.play("move")
-
+	countdown.show()
 
 func _process(delta):
 	if snapped(timer.time_left, 1) != 0:
 		countdown_label.text = str(snapped(timer.time_left, 1))
 	else:
 		countdown_label.text = 'Go!'
+		
+func _start_game() -> void:
+	countdown.hide()
+	countdown_label.hide()
+	set_processes([parallax], true)
+	animated_egg.play("move")
 
 func set_processes(nodes: Array[Node], enabled: bool) -> void:
 	for node in nodes:
@@ -33,4 +32,4 @@ func set_processes(nodes: Array[Node], enabled: bool) -> void:
 
 
 func _on_timer_timeout() -> void:
-	start_game()
+	_start_game()
