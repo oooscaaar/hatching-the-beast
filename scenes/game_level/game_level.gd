e928459f98f78890a83815ec4b99936275b9da9b
Oscar Mellado <oscar-mellado@uoc.edu>
2023-06-17 18:03:01 +0200

    WIP: Add gameover scene

--- scenes/game_level/game_level.gd
+++ scenes/game_level/game_level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player := $ParallaxBackground/Player
 @onready var parallax := $ParallaxBackground
+@onready var platforms := $ParallaxBackground/Platforms
 @onready var timer  := $Timer
 @onready var countdown := $Layout/Countdown
 @onready var animated_egg := $Layout/AnimatedEgg
@@ -9,7 +10,7 @@ extends Node2D
 
 
 func _ready():
-	set_processes([parallax], false)
+	_set_processes([parallax], false)
 	animated_egg.stop()
 	timer.start()
 	countdown.show()
@@ -19,17 +20,35 @@ func _process(delta):
 		countdown_label.text = str(snapped(timer.time_left, 1))
 	else:
 		countdown_label.text = 'Go!'
-		
+
+
 func _start_game() -> void:
 	countdown.hide()
 	countdown_label.hide()
-	set_processes([parallax], true)
+	_set_processes([parallax], true)
 	animated_egg.play("move")
 
-func set_processes(nodes: Array[Node], enabled: bool) -> void:
+
+func _gameover() -> void:
+	_hide_nodes([$Layout])
+	$ParallaxBackground/Platforms.queue_free()
+	$Gameover.show()
+	
+	
+
+func _hide_nodes(nodes: Array[Node]) -> void:
+	for node in nodes:
+		node.hide()
+
+
+func _set_processes(nodes: Array[Node], enabled: bool) -> void:
 	for node in nodes:
 		node.set_process(enabled)
 
 
 func _on_timer_timeout() -> void:
 	_start_game()
+
+
+func _on_death_area_body_exited(body):
+	_gameover()
