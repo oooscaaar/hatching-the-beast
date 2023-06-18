65de046d2eda7920f84242df6a6b12dc58ed7dcf
Oscar M <empanaisimo@gmail.com>
2023-06-18 05:27:27 +0200

    fix: intro animation offset

--- scenes/intro/intro.gd
+++ scenes/intro/intro.gd
@@ -9,6 +9,9 @@ func _on_quit_pressed() -> void:
 
 
 func _on_animated_intro_animation_finished() -> void:
+	$ParallaxBackground/StaticMeteors.set_visible(false)
+	$ParallaxBackground/StaticMeteors2.set_visible(false)
+	$ParallaxBackground/Meteors/MeteorsTexture.set_visible(true)
 	$ParallaxBackground/Meteors.set_motion_scale(Vector2(0.5,0))
 	$ParallaxBackground/PlayBtn.set_visible(true)
 	$ParallaxBackground/QuitBtn.set_visible(true)
